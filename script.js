275eff737f85d238edde53a811be9752566eee87
Jatin3180 <jatinyadav31801@gmail.com>
2024-12-03 11:06:09 +0530

    loader set

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const navlinkEL = document.querySelectorAll('.nav_link');
+// const navlinkEL = document.querySelectorAll('.nav_link');
 
-navlinkEL.forEach(navlink => {
-    navlinkEL.addEventListener('click', () =>{
-        document.querySelector('.active')?.classList.remove('active');
-        navlinkEL.classList.add('active');
-    });
-});
+// navlinkEL.forEach(navlink => {
+//     navlinkEL.addEventListener('click', () =>{
+//         document.querySelector('.active')?.classList.remove('active');
+//         navlinkEL.classList.add('active');
+//     });
+// });
+
+let s = setInterval(() => {
+    console.log("hello")
+}, 2000);
+
+
